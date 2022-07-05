658c9c33812f31f6b8a744844cf78e0676a73156
a_polumestny <anpolumestny@gmail.com>
2022-07-05 21:38:59 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,77 @@
+import argparse
+import os
+import requests
+
+from dotenv import load_dotenv
+from typing import Dict, Optional
+from urllib.parse import urlparse
+
+load_dotenv()
+
+
+def generate_headers(token: str) -> Dict[str, str]:
+    return {'Authorization': f'Bearer {token}'}
+
+
+def is_bitlink(url: str, headers: Dict[str, str]) -> bool:
+    api_url = 'https://api-ssl.bitly.com/v4/bitlinks/'
+    resp = requests.get(f'{api_url}{url}', headers=headers)
+    return resp.ok
+
+
+def parse_url(url: str) -> str:
+    parse = urlparse(url)
+    netloc = parse.netloc
+    path = parse.path
+    return f'{netloc}{path}'
+
+
+def count_clicks(bitlink: str, headers: Dict[str, str]) -> str:
+    url = f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary'
+    params = (
+        ('unit', 'month'),
+        ('units', '-1'),
+    )
+    resp = requests.get(url, headers=headers, params=params)
+    resp.raise_for_status()
+    return resp.json()['total_clicks']
+
+
+def shorten_link(url: str, headers: Dict[str, str]) -> str:
+    api_url = 'https://api-ssl.bitly.com/v4/shorten'
+    payload = {
+        'group_guid': 'Bm6363FiCth',
+        'domain': 'bit.ly',
+        'long_url': f'{url}'
+    }
+    resp = requests.post(api_url,
+                         headers=headers,
+                         json=payload
+                         )
+
+    resp.raise_for_status()
+    return resp.json()['link']
+
+
+def main(url: str):
+
+    token = os.getenv('BIT_ACCESS_TOKEN')
+    headers = generate_headers(token)
+    try:
+        bitlink = parse_url(url)
+        if is_bitlink(bitlink, headers):
+            print('short url provided')
+            click_count = count_clicks(bitlink, headers)
+            print(f'Total clicks count: {click_count}')
+        else:
+            short_url = shorten_link(url, headers)
+            print(short_url)
+    except requests.exceptions.HTTPError as err:
+        print(f'an error occurred\n{err}')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('url', help='URL to be shorten')
+    args = parser.parse_args()
+    main(args.url)
---
+++ redme.md
@@ -0,0 +1,15 @@
+# Bitly url shorterer
+
+this script gets the URL as an argument. If ULR is bitlink script return count of click, otherwise return shortened URL
+
+### How to install
+
+Python3 should be already installed. 
+Then use `pip` (or `pip3`, if there is a conflict with Python2) to install dependencies:
+```
+pip install -r requirements.txt
+```
+
+### Project Goals
+
+The code is written for educational purposes on online-course for web-developers [dvmn.org](https://dvmn.org/).
